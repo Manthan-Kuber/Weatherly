a7924ff254fdd1cf3bc6ff820c55fb0653450330
Manthan Kuber <manthankuber873@gmail.com>
2021-09-19 00:50:41 +0530

    commit2

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import requests
-from  flask import  Flask,render_template,request,redirect
-
-
-
+from  flask import  Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -12,7 +9,6 @@ def home():
     url='http://api.openweathermap.org/data/2.5/weather?q={}&appid=ff126d38ab3a4c8abd524f51e164795c'
     city=""
     weather={}
-    
     if request.method == 'POST':
         city = request.form.get('city')
         resp = requests.get(url.format(city)).json()
